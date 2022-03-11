32b6e20b61da6baf76ab7e9a382824f9d8c9eceb
John Ruddock <john.jxr@pm.me>
2022-03-11 13:17:01 -0500

    Completed Task 6, created function to add artists into the selection

--- index.js
+++ index.js
@@ -290,11 +290,12 @@ Use addArtist to do the following:
 4. Return the array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, name, years, genre, nationality, bio) {
+  array.push({name, years, genre, nationality, bio});
+  return array;
 }
 
-
+console.log('Task 6:', addArtist(artists, "John R", "1995 - 2022", "Full Stack Dev", "Canadian", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis."));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use lotsOfArt to do the following: 
